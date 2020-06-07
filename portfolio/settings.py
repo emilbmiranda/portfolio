b6935251d5b3843b797306af0469ad15324192cb
Emil Miranda <emilbmiranda@gmail.com>
2020-06-06 23:42:46 -0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+9f2e)+d-q6-cg@#*0h0r+#t9+17j-s5!3qltw2b#33vt)*-=a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emirandaportfolio.herokuapp.com']
 
 
 # Application definition
